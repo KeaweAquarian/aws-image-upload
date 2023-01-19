e461a31f8b1bab1627a3021f64a23d7bc42d75d2
KeaweAquarian <keaweaquarian@gmail.com>
2023-01-19 16:20:45 -0500

    update

--- src/main/java/com/keaweaquarian/awsimageupload/profile/UserProfileController.java
+++ src/main/java/com/keaweaquarian/awsimageupload/profile/UserProfileController.java
@@ -1,5 +1,6 @@
 package com.keaweaquarian.awsimageupload.profile;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,13 @@ import java.util.List;
 @RestController
 @RequestMapping("api/v1/user-profile")
 public class UserProfileController {
+
+    private final UserProfileService userProfileService;
+
+    @Autowired
+    public UserProfileController(UserProfileService userProfileService) {
+        this.userProfileService = userProfileService;
+    }
 
     @GetMapping
     public List<UserProfile> getUserProfiles(){
